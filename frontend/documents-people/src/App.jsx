10871bfa8ce7ec005d404fd91889446bc48ddefa
brenofroes <bfroes58@gmail.com>
2022-06-14 19:14:03 -0300

    Initial commit

---
+++ frontend/documents-people/src/App.jsx
@@ -0,0 +1,8 @@
+import './index.css'
+import React from 'react'
+import Header from './components/layout/Header'
+
+export default props => 
+  <div className='container min-h-screen h-auto flex justify-center items-center'>
+    <Header></Header>
+  </div>
